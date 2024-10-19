cadee970e14b7030859effb62b5f0749ae63d32f
Алексей <kotbarsik96@apd.dev>
2024-10-19 18:56:13 +0400

    1. Обновлять url query при обновлении фильтров 2. При первой загрузке страницы выставить значения фильтров из url query

--- stores/catalogStore.ts
+++ stores/catalogStore.ts
@@ -5,6 +5,7 @@ import type IFilterItem from '~/domain/product/types/IFilterItem'
 
 export const useCatalogStore = defineStore('catalogStore', () => {
   const router = useRouter()
+  const route = useRoute()
   const productsService = new Product()
 
   const filtersArr = ref<IFilterItem[] | null>()
@@ -17,36 +18,71 @@ export const useCatalogStore = defineStore('catalogStore', () => {
   })
   const filterValues: Record<string, any> = ref({})
 
+  watch(filterValues, updateUrlQuery, { deep: true })
+
   async function getFilters() {
     const { data } = await productsService.getCatalogFilters()
     filtersArr.value = data
     mapFilterValuesToInputs()
   }
+  /** выставит значения фильтров
+   * попытается выставить из query параметра роута, если есть
+   * если нет - выставит первые из filterValues (для radio, range)
+   */
   function mapFilterValuesToInputs() {
     if (filtersArr.value) {
       filtersArr.value.forEach((filterItem) => {
-        if (!filterValues.value[filterItem.slug]) {
-          switch (filterItem.type) {
-            case 'radio':
-              filterValues.value[filterItem.slug] =
-                filterItem.values[0]?.value_slug || ''
-              break
-            case 'checkbox':
-              filterValues.value[filterItem.slug] = []
-              break
-            case 'checkbox_boolean':
-              filterValues.value[filterItem.slug] = false
-              break
-            case 'range':
-              filterValues.value[filterItem.slug] = [
-                Math.floor(filterItem.min || 0),
-                Math.floor(filterItem.max || 0),
-              ]
-              break
-          }
+        const slug = filterItem.slug
+
+        switch (filterItem.type) {
+          case 'radio':
+            filterValues.value[slug] =
+              route.query[slug] || filterItem.values[0]?.value_slug || ''
+            break
+          case 'checkbox':
+            filterValues.value[slug] =
+              typeof route.query[slug] === 'string'
+                ? [route.query[slug]]
+                : route.query[slug] || []
+            break
+          case 'checkbox_boolean':
+            filterValues.value[slug] =
+              route.query[slug] === 'false' ? false : !!route.query[slug]
+            break
+          case 'range':
+            let [min, max] = getRangeValues(slug, filterItem)
+            filterValues.value[slug] = [Math.floor(min), Math.floor(max)]
+            break
         }
       })
     }
+  }
+  function getRangeValues(slug: string, filterItem: IFilterItem): number[] {
+    let rangeValues
+
+    // пришёл массив двух чисел (но в query они в виде строк)
+    if (Array.isArray(route.query[slug]))
+      rangeValues = route.query[slug].map((str) => Number(str))
+    // пришло только одно строковое число
+    else {
+      const num = Number(route.query[slug]) || 0
+      rangeValues = [num, num]
+    }
+
+    // далее проверить, что не меньше min и не больше max, и не перекрывают друг друга
+    if (filterItem.min && rangeValues[0] < filterItem.min)
+      rangeValues[0] = filterItem.min
+    if (filterItem.max && rangeValues[1] > filterItem.max)
+      rangeValues[1] = filterItem.max
+    if (rangeValues[0] > rangeValues[1]) rangeValues[0] = rangeValues[1]
+
+    return rangeValues
+  }
+  function updateUrlQuery() {
+    router.push({
+      name: route.name,
+      query: toValue(filterValues),
+    })
   }
 
   return {
