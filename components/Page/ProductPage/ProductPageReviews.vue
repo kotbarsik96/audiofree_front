8f81bc91413c334249707f5d8ea6b912b5590369
Алексей <kotbarsik96@apd.dev>
2024-11-03 11:21:19 +0400

    Поправил скелет =)

--- components/Page/ProductPage/ProductPageReviews.vue
+++ components/Page/ProductPage/ProductPageReviews.vue
@@ -2,11 +2,15 @@
   <div class="product-reviews">
     <ReviewForm class="product-reviews__form" />
     <div v-if="reviews.length > 0" class="product-reviews__comments">
-      <div v-for="comment in reviews" class="product-reviews__comment"></div>
+      <ReviewComment
+        v-for="comment in reviews"
+        class="product-reviews__comment"
+        :review="comment"
+      />
     </div>
     <div v-else class="product-reviews__empty">
       <EmptyBoxIcon class="catalog-body__empty-icon" />
-      <p>Отзывов еще нет. Напишите его первым!</p>
+      <p>Отзывов еще нет. Напишите первым!</p>
     </div>
   </div>
 </template>
@@ -14,6 +18,7 @@
 <script setup lang="ts">
 import EmptyBoxIcon from '~/assets/images/icons/empty-box.svg'
 import ReviewForm from '~/components/Blocks/Review/ReviewForm.vue'
+import ReviewComment from '~/components/Blocks/Review/ReviewComment.vue'
 import type { IProductReview } from '~/domain/product/types/IProductData'
 
 const route = useRoute()
