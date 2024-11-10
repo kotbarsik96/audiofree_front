3470dda03b7aa36e2efea0e6a82c2ebec3e75033
Алексей <kotbarsik96@apd.dev>
2024-11-10 11:24:04 +0400

    Ещё tabindex

--- components/Blocks/RadioLink.vue
+++ components/Blocks/RadioLink.vue
@@ -1,14 +1,20 @@
 <template>
-  <NuxtLink class="radio">
+  <NuxtLink class="radio" :tabindex="tabindex">
     <span class="radio__circle"></span>
     <span class="radio__label">{{ label }}</span>
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-  label: string
-}>()
+const props = withDefaults(
+  defineProps<{
+    label: string
+    tabindex?: '0' | '1' | '-1'
+  }>(),
+  {
+    tabindex: '0',
+  }
+)
 </script>
 
 <style lang="scss" scoped>
