e0862684a7f48bc942e39b529d004fce6b41b113
Алексей <kotbarsik96@gmail.com>
2025-03-12 20:45:13 +0400

    a_f#35 Убраны ненужные файлы

--- domain/product/types/IInjectFiltersData.ts
+++
@@ -1,7 +0,0 @@
-import type IFilterItem from '~/domain/product/types/IFilterItem'
-
-export interface IInjectFiltersData {
-  filters: Ref<Record<string, IFilterItem>>
-  filterValues: Ref<Record<string, any>>,
-  updateFilters: (slug: string, value: any) => void
-}
